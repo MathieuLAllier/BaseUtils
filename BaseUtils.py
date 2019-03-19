24ce7e890bdf987f3ef8af6f133cd134446af916
Mathieu <mathieu.lallier@quanticmind.com>
2019-03-19 18:05:02 -0400

    + complex_flatten function

--- BaseUtils.py
+++ BaseUtils.py
@@ -1,8 +1,8 @@
 from itertools import chain, islice
+from collections.abc import Iterable
 
 
 def batch(iterable, size):
-
     """
     :param iterable: Iterable (list, dict, sets, tuples)
     :param size: Size of batch. The size of the iterable to iterate over
@@ -17,4 +17,22 @@ def batch(iterable, size):
 
 
 def flatten(iterable):
+    """ Flatten a simple List of List """
     return [val for sublist in iterable for val in sublist]
+
+
+def complex_flatten(iterable):
+    """ Flatten a complex iterable """
+    """ https://stackoverflow.com/questions/2158395/flatten-an-irregular-list-of-lists """
+    for _ in iterable:
+        if isinstance(_, Iterable) and not isinstance(_, (str, bytes)):
+            yield from complex_flatten(_)
+        else:
+            yield _
+
+
+if __name__ == '__main__':
+    x = [1, [1, 2, [1, 2, 3]], [3, 4, 5]]
+    print(
+        list(complex_flatten(x))
+    )
